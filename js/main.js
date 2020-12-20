ea298129f71e1a47824b198eec17bd7ef80e983f
Khoirul22 <63836570+Khoirul-Miftakhul1@users.noreply.github.com>
2020-12-20 13:49:24 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,3 +48,26 @@ window.onload = function () {
     }
     window.setInterval(changeImage, 5000);
 }
+
+// test to add preload
+function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+preloadImages(["./images/aurora.jpg", "./images/Senja.jpg", "./images/city.jpg", "./images/night.jpg", "./images/sea.jpg", "./images/bs2.jpg"]);
